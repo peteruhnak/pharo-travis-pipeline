a356ca2c192e92e7e4c04e0c931e620e412ed43b
Peter Uhnak <i.uhnak@gmail.com>
2018-05-14 13:28:26 +0200

    deploy

--- .ssh/load-key.sh
+++ .ssh/load-key.sh
@@ -1,5 +1,3 @@
-openssl aes-256-cbc -K $encrypted_586a89c8550f_key -iv $encrypted_586a89c8550f_iv -in .ssh/1.0.1.txt.enc -out .ssh/1.0.1.txt -d
-cat .ssh/1.0.1.txt
 openssl aes-256-cbc -K $encrypted_586a89c8550f_key -iv $encrypted_586a89c8550f_iv -in .ssh/id_travis_ptp.enc -out .ssh/id_travis_ptp -d
 chmod 0400 .ssh/id_travis_ptp
 eval $(ssh-agent -s)
