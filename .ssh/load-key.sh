adc1a2654a5816cb451cfef19ab635708ce3654c
Peter Uhnak <i.uhnak@gmail.com>
2018-05-14 12:10:08 +0200

    bump

--- .ssh/load-key.sh
+++ .ssh/load-key.sh
@@ -1,7 +1,6 @@
-ls .
-ls ~
-test -e .ssh/id_travis_ptp.enc
-test -e id_travis_ptp.enc
+pwd .
+if [ -e .ssh/id_travis_ptp.enc ]; then echo "subdir"; fi
+if [ -e id_travis_ptp.enc ]; then echo "dir"; fi
 openssl aes-256-cbc -K $encrypted_586a89c8550f_key -iv $encrypted_586a89c8550f_iv -in .ssh/id_travis_ptp.enc -out .ssh/id_travis_ptp -d
 chmod 0400 .ssh/id_travis_ptp
 eval $(ssh-agent -s)
